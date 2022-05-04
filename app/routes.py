b87d3cef3078ca9705845e3bda678374c19c8fb4
Grady04 <grady.dickerson04@gmail.com>
2022-05-04 15:13:23 -0500

    added login forms

---
+++ app/routes.py
@@ -0,0 +1,51 @@
+from flask import render_template, request
+from .forms import LoginForm
+import requests
+from app import app
+
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html.j2')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+
+    if request.method == 'POST' and form.validate_on_submit():
+        # all my login stuff
+        email = form.email.data.lower() #form no longer comes from bootstrap
+        password = form.password.data #also use .get to grab stuff 
+        if email in app.config.get('REGISTERED_USERS') and password == app.config.get('REGISTERED_USERS').get(email).get('password'):
+            # login success
+            return f"Welcome {app.config.get('REGISTERED_USERS').get(email).get('name')}"
+        error_string = "incorrect email or password"
+        return render_template('login.html.j2', error=error_string, form=form)
+
+    return render_template('login.html.j2', form=form)
+
+@app.route('/pokemon', methods=['GET', 'POST'])
+def pokemon():
+    if request.method == 'POST':
+        pokemon_name = request.form.get('pokemon_name') # remember property called name in bootstrap
+        
+        url = f'https://pokeapi.co/api/v2/pokemon/{pokemon_name}'
+        response = requests.get(url)
+        if not response.ok:
+            return "We're Blasting off again!"
+        data = response.json()
+        pokedex_entry = {
+            "name": data['name'],
+            "base_exp" : data['base_experience'],
+            "sprites": data['sprites']['front_shiny'],
+            "ability": data['abilities'][0]['ability']['name'],
+            "hp": data['stats'][0]['base_stat'],
+            "attack": data['stats'][1]['base_stat'],
+            "defense": data['stats'][2]['base_stat'],
+        }
+        
+        return render_template('pokemon.html.j2', pokedex_entry=pokedex_entry)
+
+    return render_template('pokemon.html.j2')
